8f7948b97d3f98c74ac535f51d7266f4d9a6f87e
Jerry Kwok <jerrykwok.khh@gmail.com>
2024-12-07 12:50:03 +0800

    fix: production build assets path

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ declare module "@remix-run/cloudflare" {
 }
 
 export default defineConfig({
+  build: {
+    assetsDir: "app/linktr/assets",
+  },
   plugins: [
     remixCloudflareDevProxy(),
     remix({
