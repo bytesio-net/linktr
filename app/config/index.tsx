47f47c1f0ff3939907e4c3a17f8599922df0f203
jerrykhh <jerrykwok.khh@gmail.com>
2026-02-20 10:36:04 +0000

    feat: dai0wong-linktr

--- app/config/index.tsx
+++ app/config/index.tsx
@@ -2,82 +2,58 @@ import { Config } from "~/types/Config";
 
 export const config = {
   icon: {
-    light: "/app/linktr/static/logo-light.png",
-    dark: "/app/linktr/static/logo-dark.png",
+    light: "/app/linktr/static/logo-light.jpg",
+    dark: "/app/linktr/static/logo-dark.jpg",
   },
-  background: {
-    light: "/app/linktr/static/background-light.png",
-    dark: "/app/linktr/static/background-dark.png",
-  },
-  title: "Linktr - BytesIO.net",
-  description: "Bytesio.net open source linktree",
-  username: "@bytesio.net",
-  announcements: "Hello World, \n\nThis is a test announcement",
+  // background: {
+  //   light: "/app/linktr/static/background-light.png",
+  //   dark: "/app/linktr/static/background-dark.png",
+  // },
+  title: "Linktr - dai0wong",
+  description: "Illustrator || Animator",
+  username: "@dai0wong",
+  // announcements: "Hello World, \n\nThis is a test announcement",
   meta: {
     description: "Bytesio.net open source linktree",
-    "og:title": "Linktr - Bytesio.net",
-    "og:description": "Bytesio.net open source linktree",
-    "og:image":
-      "https://demo-linktr.bytesio.net/app/linktr/static/logo-light.png",
-    "og:image:alt": "Bytesio.net",
-    "og:image:width": "1280",
-    "og:image:height": "1280",
-    "og:url": "https://demo-linktr.bytesio.net/",
-    "og:locale": "en_HK",
-    "og:site_name": "Linktr - Bytesio.net",
+    "og:title": "Linktr - dai0wong 大黃 | 香港插畫家",
+    "og:description": "Illustrator || Animator",
+    "og:image": "https://dai0wong.bytesio.net/app/linktr/static/logo-light.jpg",
+    "og:image:alt": "dai0wong 大黃",
+    "og:image:width": "871",
+    "og:image:height": "871",
+    "og:url": "https://dai0wong.bytesio.net/",
+    "og:locale": "zh_HK",
+    "og:site_name": "Linktr - dai0wong 大黃 | 香港插畫家",
     "og:type": "website",
     "twitter:card": "summary_large_image",
     "twitter:title": "Linktr - Bytesio.net",
-    "twitter:description": "Bytesio.net open source linktree",
+    "twitter:description": "Illustrator || Animator",
     "twitter:image":
-      "https://demo-linktr.bytesio.net/app/linktr/static/logo-light.png",
-    "twitter:image:alt": "Bytesio.net",
-    "twitter:url": "https://demo-linktr.bytesio.net/",
+      "https://dai0wong.bytesio.net/app/linktr/static/logo-light.jpg",
+    "twitter:image:alt": "dai0wong 大黃",
+    "twitter:url": "https://dai0wong.bytesio.net/",
     keywords:
-      "Linktr, Bytesio.net, Bytesio, linktree, open source, open source linktree, open source linktree, self-host, linktrr own by yourself",
+      "Linktr, dai0wong, daiwong, 大黃, 大王, 大黃 link, Illustrator, Animator, Adobe, 香港插畫家, 香港本土, 香港本土插畫家, 香港動畫, 香港, HK",
   },
   share: {
     label: true,
   },
   layout: {
-    compact: false,
+    compact: true,
   },
   routes: {
-    shop: {
-      route: "https://instagram.com/select.conceptlab.hk",
-      label: "Shop",
-    },
     instagram: {
-      route: "https://instagram.com/",
+      route: "https://www.instagram.com/dai0wong/",
       label: "Instagram",
     },
-    facebook: {
-      route: "https://facebook.com/",
-      label: "facebook",
-    },
     threads: {
-      route: "https://threads.net/",
-      label: "@username",
-      disabled: true,
+      route: "https://threads.net/dai0wong",
+      label: "@dai0wong",
     },
     x: {
-      route: "https://x.com/",
+      route: "https://x.com/dai_w169",
       label: "X",
-      disabled: true,
-    },
-    whatsapp: {
-      route: "https://wa.me/",
-      label: "Whatsapp",
-      disabled: true,
-    },
-    repo: {
-      route: "https://github.com/bytesio-net/linktr",
-      label: "powered by bytesio.net (linktr)",
-    },
-  },
-  thirdParty: {
-    ga4: {
-      id: "G-G3ZW2G80MV",
+      // disabled: true,
     },
   },
 } as Config;
